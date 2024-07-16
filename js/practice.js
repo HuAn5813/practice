d20b2c88f647f03e8ac9fc9f094f4f38dd9accc5
HUAN <m2006871217@gmail.com>
2024-07-16 22:51:47 +0800

    1130716

--- js/practice.js
+++ js/practice.js
@@ -8,13 +8,19 @@ function jumpalt2() {
 
 }
 
-//practice2 
 function order_info() {
-    var name = document.getElementById('order_name').value;
-    var num = document.getElementById('order_num').value;
-    var mail = document.getElementById('order_mail').value;
+    //practice2 
+    var name = document.getElementById('member_name').value;
+    var phone = document.getElementById('member_phone').value;
+    var mail = document.getElementById('member_mail').value;
 
-    alert(name + '\n您好！謝謝您對我們的衣服有興趣！以下是您的訂購資訊\n電話：' + num + '\nEmail：' + mail + '\n\n感謝您的訂購！');
+    alert(name + '\n您好！謝謝您對我們的衣服有興趣！以下是您的訂購資訊\n電話：' + phone + '\nEmail：' + mail + '\n\n感謝您的訂購！');
+
+
+    //practice3
+    document.getElementById('order_name').textContent = name;
+    document.getElementById('order_phone').textContent = phone;
+    document.getElementById('order_mail').textContent = mail;
+
 }
 
-//practice3
